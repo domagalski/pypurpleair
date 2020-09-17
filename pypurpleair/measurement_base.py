d28237f9ed7a5fee6817a8c2fe3e339f5b13c170
Rachel Domagalski <rsdomagalski@gmail.com>
2020-09-16 21:50:57 -0700

    prevent corrected pm2.5 from going negative

--- pypurpleair/measurement_base.py
+++ pypurpleair/measurement_base.py
@@ -89,7 +89,9 @@ class MeasurementBase(abc.ABC):
         # Using the equation on page 8 of the EPA report pdf
         # constants on that page are different than at the end for some reason.
         pm2_5_mean = (pm2_5_cf_1_a + pm2_5_cf_1_b) / 2
-        return 0.52 * pm2_5_mean - 0.085 * humidity + 5.71
+        # It's possible when pm2_5 is near zero and the humidty is high that pm2.5
+        # could go negative after correction. Assume anything less than zero is zero.
+        return max(0, 0.52 * pm2_5_mean - 0.085 * humidity + 5.71)
 
     @property
     def pm2_5_epa_correction(self) -> Optional[float]:
